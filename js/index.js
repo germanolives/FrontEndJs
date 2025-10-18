4b87a2a332137d9a5b4061d0f5dfc16b27a4da8a
Germán Matías Olives <germanolives@gmail.com>
2025-10-18 12:28:00 -0300

    NuevaToggleUnica

--- js/index.js
+++ js/index.js
@@ -1,58 +1,106 @@
-const elementoH1 = document.getElementById('titulo');
-const elementoSearch = document.getElementById('search');
-const botonLupaSwitchOff = document.getElementById('lupaToggleOff');
-const botonLupaSwitchOn = document.getElementById('lupaToggleOn');
-const botonLunaSwitchOff = document.getElementById('lunaToggleOff');
-const botonLunaSwitchOn = document.getElementById('lunaToggleOn');
-const elementoBody = document.querySelector('body');
-const elementoNav = document.querySelector('.nav');
-const elementoLogo = document.querySelector('.logo');
-const elementoTyc = document.querySelector('.tyc');
-const elementoThreads = document.querySelector('.threads');
-const elementoTiktok = document.querySelector('.tiktok');
-const elementoTwitter = document.querySelector('.twitter');
-const elementoClassH1 = document.querySelector('.h1');
-const elementoContenedorCategorias = document.querySelector('.categorias');
-const elementoContenedorAceites = document.querySelector('.aceites');
-const elementoContenedorFrutosSecos = document.querySelector('.frutosSecos');
-const elementoContenedorSemillas = document.querySelector('.semillas');
-const elementosToggle = document.querySelectorAll('.toggle');
-const elementosToggleOn = document.querySelectorAll('.toggleOn');
+const h1 = document.getElementById('titulo');
+const search = document.getElementById('search');
+const claroOscuro = document.getElementById('boton-claroOscuro');
+const tituloBusqueda = document.getElementById('boton-tituloBusqueda');
 
-let verOcultarLupa = function() {
-  elementoH1.classList.toggle('oculto');
-  elementoSearch.classList.toggle('visible');
-  botonLupaSwitchOff.classList.toggle('oculto');
-  botonLupaSwitchOn.classList.toggle('visible');
-}
+ let switchTituloBusqueda = function() {
+   h1.classList.toggle('oculto');
+   search.classList.toggle('visible');
+   tituloBusqueda.classList.toggle('activo');
+ }
 
-let modoNocturno = function() {
-  elementoBody.classList.toggle('dark');
-  elementoTyc.classList.toggle('tycDark');
-  elementoThreads.classList.toggle('threadsDark');
-  elementoTiktok.classList.toggle('tiktokDark');
-  elementoTwitter.classList.toggle('twitterDark');
-  elementoClassH1.classList.toggle('h1Dark');
-  elementoNav.classList.toggle('navDark');
-  elementoLogo.classList.toggle('logoDark');
-  elementoContenedorCategorias.classList.toggle('categoriasDark');
-  elementoContenedorAceites.classList.toggle('aceitesDark');
-  elementoContenedorFrutosSecos.classList.toggle('frutosSecosDark');
-  elementoContenedorSemillas.classList.toggle('semillasDark');
-  botonLunaSwitchOff.classList.toggle('oculto');
-  botonLunaSwitchOn.classList.toggle('visible');
-  for(i=0;i<elementosToggle.length;i++) {
-    elementosToggle[i].classList.toggle('toggleDark');
-  }
-  for(i=0;i<elementosToggleOn.length;i++) {
-    elementosToggleOn[i].classList.toggle('toggleOnDark');
-  }
-}
+ tituloBusqueda.addEventListener('click', switchTituloBusqueda);
 
 
-botonLupaSwitchOff.addEventListener('click', verOcultarLupa);
-botonLupaSwitchOn.addEventListener('click', verOcultarLupa);
-botonLunaSwitchOff.addEventListener('click', modoNocturno);
-botonLunaSwitchOn.addEventListener('click', modoNocturno);
 
-console.log(elementoNav);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+// const elementoH1 = document.getElementById('titulo');
+// const elementoSearch = document.getElementById('search');
+// const botonLupaSwitchOff = document.getElementById('lupaToggleOff');
+// const botonLupaSwitchOn = document.getElementById('lupaToggleOn');
+// const botonLunaSwitchOff = document.getElementById('lunaToggleOff');
+// const botonLunaSwitchOn = document.getElementById('lunaToggleOn');
+// const elementoBody = document.querySelector('body');
+// const elementoNav = document.querySelector('.nav');
+// const elementoLogo = document.querySelector('.logo');
+// const elementoTyc = document.querySelector('.tyc');
+// const elementoThreads = document.querySelector('.threads');
+// const elementoTiktok = document.querySelector('.tiktok');
+// const elementoTwitter = document.querySelector('.twitter');
+// const elementoClassH1 = document.querySelector('.h1');
+// const elementoContenedorCategorias = document.querySelector('.categorias');
+// const elementoContenedorAceites = document.querySelector('.aceites');
+// const elementoContenedorFrutosSecos = document.querySelector('.frutosSecos');
+// const elementoContenedorSemillas = document.querySelector('.semillas');
+// const elementosToggle = document.querySelectorAll('.toggle');
+// const elementosToggleOn = document.querySelectorAll('.toggleOn');
+
+// let verOcultarLupa = function() {
+//   elementoH1.classList.toggle('oculto');
+//   elementoSearch.classList.toggle('visible');
+//   botonLupaSwitchOff.classList.toggle('oculto');
+//   botonLupaSwitchOn.classList.toggle('visible');
+// }
+
+// let modoNocturno = function() {
+//   elementoBody.classList.toggle('dark');
+//   elementoTyc.classList.toggle('tycDark');
+//   elementoThreads.classList.toggle('threadsDark');
+//   elementoTiktok.classList.toggle('tiktokDark');
+//   elementoTwitter.classList.toggle('twitterDark');
+//   elementoClassH1.classList.toggle('h1Dark');
+//   elementoNav.classList.toggle('navDark');
+//   elementoLogo.classList.toggle('logoDark');
+//   elementoContenedorCategorias.classList.toggle('categoriasDark');
+//   elementoContenedorAceites.classList.toggle('aceitesDark');
+//   elementoContenedorFrutosSecos.classList.toggle('frutosSecosDark');
+//   elementoContenedorSemillas.classList.toggle('semillasDark');
+//   botonLunaSwitchOff.classList.toggle('oculto');
+//   botonLunaSwitchOn.classList.toggle('visible');
+//   for(i=0;i<elementosToggle.length;i++) {
+//     elementosToggle[i].classList.toggle('toggleDark');
+//   }
+//   for(i=0;i<elementosToggleOn.length;i++) {
+//     elementosToggleOn[i].classList.toggle('toggleOnDark');
+//   }
+// }
+
+
+// botonLupaSwitchOff.addEventListener('click', verOcultarLupa);
+// botonLupaSwitchOn.addEventListener('click', verOcultarLupa);
+// botonLunaSwitchOff.addEventListener('click', modoNocturno);
+// botonLunaSwitchOn.addEventListener('click', modoNocturno);
+
+
